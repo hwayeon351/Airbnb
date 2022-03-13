2fe2cf811870da18803807bc741047c02023fdb1
hwayeon <hwayeon351@gmail.com>
2022-03-13 16:10:29 +0900

    Chore: Change Url for house list

--- app/src/main/java/com/example/airbnb/HouseService.kt
+++ app/src/main/java/com/example/airbnb/HouseService.kt
@@ -4,6 +4,6 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface HouseService {
-    @GET("/v3/69a88557-560d-4285-bb16-3582299033a0")
+    @GET("/v3/240eb6b9-9d63-440e-91e6-dfc773f1e6f1")
     fun getHouseList(): Call<HouseDto>
 }
